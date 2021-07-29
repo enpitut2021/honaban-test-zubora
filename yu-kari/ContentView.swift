94d5941aaef53e1a03250a251fe80485a6fb1cae
Kentaro Jay Takahashi <64148935+KentaroJay@users.noreply.github.com>
2021-07-29 11:00:45 +0900

    Add files via upload

---
+++ yu-kari/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  yu-kari
+//
+//  Created by MacBook Pro on 2021/07/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ yu-kari/yu_kariApp.swift
@@ -0,0 +1,17 @@
+//
+//  yu_kariApp.swift
+//  yu-kari
+//
+//  Created by MacBook Pro on 2021/07/29.
+//
+
+import SwiftUI
+
+@main
+struct yu_kariApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
