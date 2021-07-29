cb61907723ffaf001ad06d0f919e2be614e54a73
KentaroJay <kentaro.jay.takahashi@gmail.com>
2021-07-29 16:31:02 +0900

    ボタンを押したらコアラが喜ぶ&ボタンのラベルが変わる

--- yu-kari/ContentView.swift
+++ yu-kari/ContentView.swift
@@ -1,16 +1,30 @@
 //
 //  ContentView.swift
-//  yu-kari
+//  koala
 //
-//  Created by MacBook Pro on 2021/07/29.
+//  Created by 森屋 優 on 2021/07/29.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State var buttonText = "ユーカリをあげる"
+    @State var imageName = "Sleeping_Koala"
+    
     var body: some View {
-        Text("Hello, swift! Checked by Mizuna")
-            .padding()
+        VStack {
+            Text("Hi!, I'm Koala")
+            Image(self.imageName)
+                .resizable()
+                .scaledToFit()
+            
+            Button(action: {
+                self.buttonText = "ユーカリをあげました"
+                self.imageName = "Smiling_Koala-1"
+            }){
+                Text(self.buttonText)
+            }
+        }
     }
 }
 
